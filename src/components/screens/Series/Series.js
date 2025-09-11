bd60110ef7570edde748abf9ef7ffa2f6d7dc245
BrisaPereyra23 <bpereyra@udesa.edu.ar>
2025-09-11 11:21:39 -0300

    hice de nuevo series

--- src/components/screens/Series/Series.js
+++ src/components/screens/Series/Series.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Series extends Component {
   constructor(props) {
@@ -7,35 +7,98 @@ class Series extends Component {
     this.state = {
       series: [],
       cargando: true,
-      error: null
+      error: null,
+      page: 1,
+      filter: ""
     };
   }
 
   componentDidMount() {
-    fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=&api_key=eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyODljZWFiM2FjYTZhMmZhZTYzYWEzMTUzZDk1YWI0YiIsIm5iZiI6MTc1NzI1NjE1MC44OTEsInN1YiI6IjY4YmQ5OWQ2YjRiM2JiYjczYjliYzIyNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hn2Sy_eCyWgGOCC7J-_Mo0__KiLW-6hq_rrR54C6gzI`)
-      .then(res => res.json())
-      .then(data => this.setState({ series: data.results, cargando: false }))
-      .catch(err => this.setState({ error: err.message, cargando: false }));
+    this.fetchSeries();
   }
 
+  fetchSeries = () => {
+    fetch(
+      `https://api.themoviedb.org/3/tv/popular?language=es-ES&page=${this.state.page}&api_key=eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyODljZWFiM2FjYTZhMmZhZTYzYWEzMTUzZDk1YWI0YiIsIm5iZiI6MTc1NzI1NjE1MC44OTEsInN1YiI6IjY4YmQ5OWQ2YjRiM2JiYjczYjliYzIyNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hn2Sy_eCyWgGOCC7J-_Mo0__KiLW-6hq_rrR54C6gzI`
+    )
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+           series: data.results, 
+           cargando: false
+        })
+      )
+      .catch((err) => this.setState({ err, cargando: false })
+      );
+  };
+/*ver de aca */
+  cargarMas = () => {
+    this.setState(
+      { page: this.state.page + 1, cargando: true },
+      this.fetchSeries
+    );
+  };
+
+  manejarFiltro = (e) => {
+    this.setState({ filter: e.target.value });
+  };
+/*a aca */
   render() {
-    if (this.state.cargando) return <p>Cargando series...</p>;
-    if (this.state.error) return <p>Error: {this.state.error}</p>;
+    const series = this.state.series;
+    const cargando = this.state.cargando;
+    const error = this.state.err;
+    const filter = this.state.filter;
+
+    if (cargando && series.length === 0) 
+      return <p>Cargando series...</p>;
+    if (error) 
+      return <p>Error: {err}</p>;
+
+    const seriesFiltradas = series.filter((show) =>
+      show.name.toLowerCase().includes(filter.toLowerCase())
+    );
 
     return (
-      <div>
+      <div className="container">
         <h1>Series Populares</h1>
-        <ul>
-          {this.state.series.map(show => (
-            <li key={show.id}>
-              <Link to={`/detail/${show.id}`}>{show.name}</Link>
-            </li>
+
+        <form className="filter-form px-0 mb-3">
+          <input
+            type="text"
+            name="filter"
+            placeholder="Buscar dentro de la lista"
+            value={filter}
+            onChange={this.manejarFiltro}
+          />
+        </form>
+
+        <section className="row cards all-series" id="series">
+          {seriesFiltradas.map((show) => (
+            <article className="single-card-serie col-md-3 mb-4" key={show.id}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${show.poster_path}`}
+                className="card-img-top"
+                alt={show.name}
+              />
+              <div className="cardBody">
+                <h5 className="card-title">{show.name}</h5>
+                <p className="card-text">
+                  {show.name.toUpperCase()}
+                </p>
+                <Link to={`/detail/tv/${show.id}`} className="btn btn-primary">
+                  Ver más
+                </Link>
+              </div>
+            </article>
           ))}
-        </ul>
+        </section>
+
+        <button className="btn btn-info" onClick={this.cargarMas}>
+          Cargar más
+        </button>
       </div>
     );
   }
 }
 
 export default Series;
-
