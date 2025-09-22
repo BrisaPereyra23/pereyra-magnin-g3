6f1e76b1988739cbf8e32466df31b98ca0806e58
BrisaPereyra23 <bpereyra@udesa.edu.ar>
2025-09-22 14:57:32 -0300

    arregle para que se vean las fotos de favorites

--- src/components/screens/Favorites/Favorites.js
+++ src/components/screens/Favorites/Favorites.js
@@ -65,7 +65,7 @@ class Favorites extends Component {
                     className="card-img"
                     src={
                       fav.poster_path
-                        ? `https://image.tmdb.org/t/p/w500 ${fav.poster_path}`
+                        ? `https://image.tmdb.org/t/p/w500${fav.poster_path}`
                         : `https://via.placeholder.com/500x750?text=Sin+imagen`
                     }
                     alt={fav.title ? fav.title : fav.name}
